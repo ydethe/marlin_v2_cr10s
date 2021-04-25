4e741c71d353f191c2ea86367099aa7ea7c71850
Yann de Thé <ydethe@gmail.com>
2021-04-25 22:11:42 +0200

    Version OK

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1314,7 +1314,7 @@
 #define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
-  #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
+  #define MESH_EDIT_Z_STEP  0.05  // (mm) Step size while manually probing Z axis.
   #define LCD_PROBE_Z_RANGE 4     // (mm) Z Range centered on Z_MIN_POS for LCD Z adjustment
   //#define MESH_EDIT_MENU        // Add a menu to edit mesh points
 #endif
