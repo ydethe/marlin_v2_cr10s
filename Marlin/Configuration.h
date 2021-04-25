9da3d55a3c6008b0e17b2d738fda1192e9e9480b
Yann de Thé <ydethe@gmail.com>
2021-04-25 22:35:14 +0200

    Version a tester

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Yann's CR10S 2"
+#define CUSTOM_MACHINE_NAME "Yann's CR10S"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
