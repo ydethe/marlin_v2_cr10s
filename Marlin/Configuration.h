a0de7b26d77137f89ffc7ccf0e37fe2ec2e1fa76
Yann de Thé <ydethe@gmail.com>
2021-04-26 14:25:15 +0200

    Autotune PID for extruder and bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -500,9 +500,9 @@
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
     // Stock CR-10 Hotend fan 100%
-    #define DEFAULT_Kp 17.42
-    #define DEFAULT_Ki 1.27
-    #define DEFAULT_Kd 59.9
+    #define DEFAULT_Kp 30.87
+    #define DEFAULT_Ki 2.43
+    #define DEFAULT_Kd 97.98
   #endif
 #endif // PIDTEMP
 
@@ -542,9 +542,9 @@
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   //Stock Bed Tuned for 50C
-  #define DEFAULT_bedKp 690.34
-  #define DEFAULT_bedKi 111.47
-  #define DEFAULT_bedKd 1068.83
+    #define DEFAULT_bedKp 54.90
+    #define DEFAULT_bedKi 10.81
+    #define DEFAULT_bedKd 185.91
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
